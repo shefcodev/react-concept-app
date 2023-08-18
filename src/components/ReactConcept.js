b5561c6a263bde723e31f8b6a1c4f71256bf9691
shefcodev <sherefjimoh@gmail.com>
2023-08-18 16:15:27 +0100

    created Header components

---
+++ src/components/ReactConcept.js
@@ -0,0 +1,11 @@
+const ReactConcept = () => {
+  return (
+    <li className="concept">
+      <img src="TODO: IMAGE" alt="TODO: TITLE" />
+      <h2>TODO: TITLE</h2>
+      <p>TODO: DESCRIPTION</p>
+    </li>
+  );
+};
+
+export default ReactConcept;
