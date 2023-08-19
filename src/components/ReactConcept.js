2d425cd0ffb18da94a89df9209700d6fbd3280f7
shefcodev <sherefjimoh@gmail.com>
2023-08-19 16:21:59 +0100

    finalized App

--- src/components/ReactConcept.js
+++ src/components/ReactConcept.js
@@ -1,7 +1,7 @@
 const ReactConcept = ({ component }) => {
   return (
    <li className="concept">
-    <img src={component.image} alt="TODO: TITLE" />
+    <img src={component.image} alt={component.title} />
     <h2>{component.title}</h2>
     <p>{component.description}</p>
   </li>
